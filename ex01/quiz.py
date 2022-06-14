bb21f1b6d55db1dc22033888bc29a3116d220ecf
C0A21022 <c0a2102277@edu.teu.ac.jp>
2022-06-14 16:16:24 +0900

    紐づけテスト #2

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,20 +1,27 @@
 import random
+import datetime
+
 def main():
     q=random.randint(0,2)
+
+    st = datetime.datetime.now()
     print("問題：")
     print(shutudai(q))
     ans1=input("答えるんだ：")
 
-    if ans1 in kaito[q] == True:
+    if (ans1 in kaito(q)) == True:
         print("正解")
     else:
         print("出直してこい")
+
+    ed = datetime.datetime.now()
+    print((ed-st).seconds)
 
 def shutudai(x):
     ques=["サザエの旦那の名前は？",
     "カツオの妹の名前は？",
     "タラオはカツオから見てどんな関係？"]
-    return (ques[x])
+    return(ques[x])
 
 def kaito(y):
     ans=[["マスオ", "ますお"],
@@ -23,5 +30,4 @@ def kaito(y):
     return ans[y]
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
