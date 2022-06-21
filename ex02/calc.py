59ee494ac14b54b3266d413c4f122b328670959b
C0A21022 <c0a2102277@edu.teu.ac.jp>
2022-06-21 16:03:40 +0900

    数字の消去を実装

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,21 +7,25 @@ root.title("電卓")
 #root.geometry("300x500")
 
 
-def button_click(event):
+def button_click(event):      #各ボタンが押された時の処理
     button = event.widget
     text = button["text"]
     #tkm.showinfo(text, f"[{text}]のボタンがクリックされました")
 
-    if text == "=":
+    if text == "=":           #=が押されたら計算結果を表示する
         eqn = entry.get()
         ans = int(eval(eqn))
         entry.delete(0,tk.END)
         entry.insert(tk.END, ans)
-    else:
+    elif text == "AC":        #ACが押されたら数字を全て消す
+        entry.delete(0,tk.END)
+    elif text == "C":         #Cが押されたら数字を一桁消す
+        entry.delete(len(entry.get())-1, tk.END)
+    else:                     #数字か符号を入力する
         entry.insert(tk.END, text)
 
 
-entry = tk.Entry(root,
+entry = tk.Entry(root,        #テキストボックスを配置する
                 justify="right", 
                 width=10,
                 font=("Times New Roman", 40)
@@ -30,7 +34,8 @@ entry.grid(row=0, column=0, columnspan=3)
 
 
 r, c , x= 1, 0 ,9
-nums=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "*", "/", "="]
+nums=["", "AC", "C", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "*", "/", "="]
+#各ボタンを配置する
 for i in nums:
     button = tk.Button (root,
                         font=("Times New Roman", 30),
