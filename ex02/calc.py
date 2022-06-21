5ae96f6044ceb8d64e8874ab8b8bb6eb2403effc
C0A21022 <c0a2102277@edu.teu.ac.jp>
2022-06-21 16:41:05 +0900

    テキストボックスを2つに増やす、レイアウトの変更

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
+
 root = tk.Tk()
 root.title("電卓")
 #root.geometry("300x500")
@@ -12,29 +13,42 @@ def button_click(event):      #各ボタンが押された時の処理
     text = button["text"]
     #tkm.showinfo(text, f"[{text}]のボタンがクリックされました")
 
-    if text == "=":           #=が押されたら計算結果を表示する
-        eqn = entry.get()
-        ans = int(eval(eqn))
-        entry.delete(0,tk.END)
-        entry.insert(tk.END, ans)
-    elif text == "AC":        #ACが押されたら数字を全て消す
-        entry.delete(0,tk.END)
-    elif text == "C":         #Cが押されたら数字を一桁消す
-        entry.delete(len(entry.get())-1, tk.END)
-    else:                     #数字か符号を入力する
-        entry.insert(tk.END, text)
+    try:
+        if text == "=":           #=が押されたら計算結果をentry2に表示する
+            eqn = entry1.get()
+            ans = (eval(eqn))
+            entry2.delete(0,tk.END)
+            entry2.insert(tk.END, ans)
+        elif text == "AC":        #ACが押されたら数字を全て消す
+            entry1.delete(0,tk.END)
+            entry2.delete(0,tk.END)
+
+        elif text == "C":         #Cが押されたら数字を一桁消す
+            entry1.delete(len(entry1.get())-1, tk.END)
+        else:                     #数字か符号を入力する
+            entry1.insert(tk.END, text)
+    except:                       #式として成り立たない時はERRORを返す
+            entry2.delete(0,tk.END)
+            entry2.insert(tk.END, "ERROR")
 
 
-entry = tk.Entry(root,        #テキストボックスを配置する
+entry1 = tk.Entry(root,        #数式を入力するテキストボックス1を配置する
                 justify="right", 
                 width=10,
                 font=("Times New Roman", 40)
                 )
-entry.grid(row=0, column=0, columnspan=3)
+entry1.grid(row=0, column=0, columnspan=4)
+
+entry2 = tk.Entry(root,        #計算結果が出力されるテキストボックス2を配置する
+                justify="right", 
+                width=10,
+                font=("Times New Roman", 40)
+                )
+entry2.grid(row=1, column=0, columnspan=4)
 
 
-r, c , x= 1, 0 ,9
-nums=["", "AC", "C", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "*", "/", "="]
+r, c , x= 2, 0 ,0
+nums=["",  "AC", "C", "/", 9, 8, 7, "*", 6, 5, 4,"-", 3, 2, 1, "+", 0, "", "", "="]
 #各ボタンを配置する
 for i in nums:
     button = tk.Button (root,
@@ -47,7 +61,7 @@ for i in nums:
 
     c+=1
     x-=1
-    if (x)%3 == 0:
+    if (x)%4 == 0:
         r += 1
         c = 0
 
