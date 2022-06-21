e147dc75c3641479f991a710d343ddb58a8e9e03
C0A21022 <c0a2102277@edu.teu.ac.jp>
2022-06-21 15:41:47 +0900

    問7

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,15 +1,25 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+
 root = tk.Tk()
 root.title("電卓")
 #root.geometry("300x500")
 
+
 def button_click(event):
     button = event.widget
     text = button["text"]
-    entry.insert(tk.END, text)
     #tkm.showinfo(text, f"[{text}]のボタンがクリックされました")
+
+    if text == "=":
+        eqn = entry.get()
+        ans = eval(eqn)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END, ans)
+    else:
+        entry.insert(tk.END, text)
+
 
 entry = tk.Entry(root,
                 justify="right", 
@@ -18,8 +28,9 @@ entry = tk.Entry(root,
                 )
 entry.grid(row=0, column=0, columnspan=3)
 
+
 r, c , x= 1, 0 ,9
-nums=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+"]
+nums=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]
 for i in nums:
     button = tk.Button (root,
                         font=("Times New Roman", 30),
