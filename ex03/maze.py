68a1f8a2599144a0a419c9b1efe50b903aa60c76
C0A21022 <c0a2102277@edu.teu.ac.jp>
2022-06-28 15:22:29 +0900

    問8, 9, 10

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -23,6 +24,9 @@ if __name__ == "__main__":
 
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+
+    maze_bg = mm.make_maze(15, 9)
+    mm.show_maze(canvas, maze_bg)
 
     tori = tk.PhotoImage(file="fig/9.png")
     cx, cy = 300, 400
