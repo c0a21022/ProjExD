890a968fcd46609ffc807678f32b9a6073808c97
C0A21022 <c0a2102277@edu.teu.ac.jp>
2022-07-05 17:18:29 +0900

    4限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,7 +1,9 @@
-from tabnanny import check
 import pygame as pg
 import sys
 import random
+import time
+import tkinter as tk
+import tkinter.messagebox as tkm
 
 def main():
     clock = pg.time.Clock()
@@ -12,14 +14,14 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.image.load("fig/3.png")
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
     kkimg_rct = kkimg_sfc.get_rect()
     kkimg_rct.center = 900, 400
 
-    bmimg_sfc = pg.Surface((20, 20))
+    bmimg_sfc = pg.Surface((60, 60))
     bmimg_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(bmimg_sfc, (255, 0, 0), (30, 30), 30)
     bmimg_rct = bmimg_sfc.get_rect()
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
     bmimg_rct.centery = random.randint(0, screen_rct.height)
@@ -34,15 +36,20 @@ def main():
             if event.type == pg.QUIT: return
 
         key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
-        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
-        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
-        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        if key_states[pg.K_LSHIFT] == True:       #LSHIFTを押している間移動速度を上げる
+            move_speed = 4
+        else:
+            move_speed = 2
+
+        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= move_speed
+        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += move_speed
+        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= move_speed
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += move_speed
         if check_bound(kkimg_rct, screen_rct) != (1,1) :
-            if key_states[pg.K_UP]    == True: kkimg_rct.centery += 1
-            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
-            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
-            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
+            if key_states[pg.K_UP]    == True: kkimg_rct.centery += move_speed
+            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= move_speed
+            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += move_speed
+            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= move_speed
 
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
@@ -51,10 +58,14 @@ def main():
         screen_sfc.blit(bmimg_sfc, bmimg_rct)
 
         yoko, tate = check_bound(bmimg_rct, screen_rct)
-        vx *= yoko
-        vy *= tate
+        vx *= yoko *1.0001                      #壁に反射する度に僅かに加速
+        vy *= tate *1.0001
 
-        if kkimg_rct.colliderect(bmimg_rct):return
+        if kkimg_rct.colliderect(bmimg_rct):
+            root=tk.Tk()
+            root.withdraw()
+            tkm.showerror("残念！", "GameOver")  #爆弾に接触するとメッセージを表示
+            return
 
         pg.display.update()
         clock.tick(1000)
